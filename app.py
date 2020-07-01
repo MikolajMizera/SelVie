fcd2378696df76fee4cfcd28d75bdd037b2bab29
MikolajMizera <mikolajmizera@gmail.com>
2020-07-01 10:51:26 +0200

    Added dynamic slider and dropdown lists

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ __status__ = 'PROTOTYPE'
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
 upload_div = html.Div([
@@ -95,9 +96,31 @@ data_table = dash_table.DataTable(id='table',
                                   data=[],
                                   merge_duplicate_headers=True)
 
+filters_div = html.Div([
+                html.Table([
+                        html.Tr([
+                                html.Td(dcc.Dropdown(id='main_rec_drop')),
+                                html.Td(dcc.Dropdown(id='secondary_rec_drop',
+                                                     multi=True))
+                                ]),
+                        html.Tr([
+                                html.Td(dcc.RangeSlider(id='sel_slider',
+                                                        min=0,
+                                                        max=1,
+                                                        step=0.01,
+                                                        value=[0, 1],
+                                                        marks={0:{'label':'0'},
+                                                               1:{'label':'1'}}),
+                                        colSpan=2),
+                                ])
+                            ], style = {'width': '100%'}),                
+                                
+                ], style={'width': '600px'})
+
 app.layout = html.Div([
         upload_div,
         dcc.Store(id='df-store', storage_type='memory'),
+        filters_div,
         data_table])
 
 
@@ -114,15 +137,66 @@ def process_upload(contents, name, date):
     else:
         return []
 
-@app.callback([Output("table", "data"), Output('table', 'columns')],
-              [Input('df-store', 'modified_timestamp')],
-              [State('df-store', 'data')])
-def update_table(ts, data):
-    if ts is None:
+@app.callback([Output('table', 'data'), 
+               Output('table', 'columns')],
+              [Input('df-store', 'data')])
+def update_table(data):
+    
+    try:
+        df = pd.read_json(data)
+    except:
         raise PreventUpdate
-    df = pd.read_json(data)
+    
     cols = [{'name': n.split('_'), 'id': n} for n in df.columns]
     return df.round(3).to_dict('records'), cols
+
+@app.callback([Output('main_rec_drop', 'options'),
+               Output('main_rec_drop', 'value')],
+              [Input('table', 'columns')])
+def update_main_rec_drop(columns):
+    
+    columns = [c['id'] for c in columns]
+    receptors = np.unique([c.split('_')[0] for c in columns])
+    options = [{'label': r, 'value': r} for r in receptors]
+    return options, receptors[0]
+
+@app.callback([Output('secondary_rec_drop', 'options'),
+               Output('secondary_rec_drop', 'value')],
+              [Input('main_rec_drop', 'value')],
+              [State('table', 'columns')])
+def update_secondary_rec_drop(main_rec, columns):
+        
+    columns = [c['id'] for c in columns]
+    receptors = np.unique([c.split('_')[0] for c in columns])
+    options = [{'label': r, 'value': r} for r in receptors if r!=main_rec]
+    return options, receptors[1:]
+
+@app.callback([Output('sel_slider', 'min'),
+               Output('sel_slider', 'max'),
+               Output('sel_slider', 'value'),
+               Output('sel_slider', 'marks')],
+              [Input('secondary_rec_drop', 'value')],
+              [State('table', 'data'),
+               State('main_rec_drop', 'value')])
+def update_sel_slider(sec_rec, selected_df, main_rec):
+        
+    if (main_rec is None) or (sec_rec is None) or (selected_df is None):
+        raise PreventUpdate
+    
+    df = pd.DataFrame(selected_df)
+    preds_df = df[[c for c in df.columns if 'prediction' in c]]
+    
+    isin_cols = lambda x: [r in x for r in sec_rec]
+    sec_rec_cols = [c for c in preds_df.columns if np.any(isin_cols(c))]
+    sec_rec_data = preds_df[sec_rec_cols]
+    
+    deltas = (-(sec_rec_data.T-preds_df['%s_prediction'%main_rec]).T).min(1)
+    max_val = deltas.max()
+    
+    min_mark, max_mark =  [0, max_val] 
+    marks = {n:{'label':'%.1f'%n} for n in np.arange(min_mark, max_mark, 0.2)} 
+    
+    return 0, max_val, [0, max_val], marks               
 
 if __name__ == '__main__':
     app.run_server(debug=False)
